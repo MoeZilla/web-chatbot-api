30771efeb89e5442c7f739ac55f89a1ba383b625
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-12 15:43:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+var server = http.createServer(app);
+const keepAlive = require('./index.js');
+
+app.get('/api/chatbot', function(request, response){
+  let user = request.query.user || "Pranav"  
+  let name = request.query.name || "Moezilla"
+  let teamname = request.query.teamname || "moezilla"
+  let gender = request.query.gender || "female"
+    fetch(`http://api.brainshop.ai/get?bid=178&key=sX5A2PcYZbsN5EY6&uid=${user}&msg=${encodeURIComponent(request.query.msg)}`)
+    .then(response => response.json())
+    .then(data => {
+        response.status(500).json({
+        msg: data.cnt.replace('Aco', `${name} bot).replace('Acobot Team', `${teamname} Team).replace('Male chatbot', `${gender} chatbot`)
+    });
+    });
+});
+
+app.get("/", (request, response) => {
+  console.log(`😂Enjoy`);
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end("Moezilla Is Best")
+});
+
+const listener = server.listen(process.env.PORT, function keepAlive() {
+  console.log(`Chatbot Api on port ` + listener.address().port);
+});
+
+module.exports = keepAlive;
+
+keepAlive();
+ 
